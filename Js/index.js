2399bfd90186dd7c8f2445c9d2f73e9c6ac41070
Luciefifi <niyomutonilucie@gmail.com>
2022-12-09 17:15:19 +0200

    worked on responsiveness

---
+++ Js/index.js
@@ -0,0 +1,17 @@
+//Phone Navigation Bar
+
+const toggleButton = document.getElementById ("toggleButton");
+const closeButton = document.getElementById("closeButton");
+const nav = document.getElementById('menu')
+nav.classList.remove('menu-btn');
+
+
+toggleButton.addEventListener ("click", function(){
+    nav.classList.add('menu-btn');
+})
+
+
+
+nav.addEventListener("click", function(){
+    nav.classList.remove('menu-btn');
+})
