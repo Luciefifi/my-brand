aab60177edfc10703a6b7d3e5bf8685b3531daee
Luciefifi <niyomutonilucie@gmail.com>
2023-02-02 11:34:19 +0300

    ft(login): -modify login.js -[finishes#004]

--- Js/login.js
+++ Js/login.js
@@ -15,71 +15,111 @@ signInButton.addEventListener("click", (event)=>{
 })
 
 function login(){
-    var userEmail = document.loginForm.email.value;
-    atpos = userEmail.indexOf("@");
-    dotpos = userEmail.lastIndexOf(".");
+//     var userEmail = document.loginForm.email.value;
+//     atpos = userEmail.indexOf("@");
+//     dotpos = userEmail.lastIndexOf(".");
   
-    if ((atpos < 1 || dotpos - atpos < 2) && userEmail == "") {
-        registeredEmailMessage.style.display = "block"
-        registeredEmailMessage.style.color = "red"
-        registeredEmailMessage.style.fontWeight = "bold"
-        registeredEmailMessage.innerHTML = "Invalid email!"
-      document.myForm.email.focus();
-    }
+//     if ((atpos < 1 || dotpos - atpos < 2) && userEmail == "") {
+//         registeredEmailMessage.style.display = "block"
+//         registeredEmailMessage.style.color = "red"
+//         registeredEmailMessage.style.fontWeight = "bold"
+//         registeredEmailMessage.innerHTML = "Invalid email!"
+//       document.myForm.email.focus();
+//     }
 
-    else if (document.loginForm.pwd.value == "" ) {
-        registeredPasswordMessage.style.display = "block"
-        registeredPasswordMessage.style.color = "red"
-        registeredPasswordMessage.style.fontWeight = "bold"
-        registeredPasswordMessage.innerHTML = "Password field is required!"
-      document.loginForm.pwd.focus();
-      return false;
-    }
+//     else if (document.loginForm.pwd.value == "" ) {
+//         registeredPasswordMessage.style.display = "block"
+//         registeredPasswordMessage.style.color = "red"
+//         registeredPasswordMessage.style.fontWeight = "bold"
+//         registeredPasswordMessage.innerHTML = "Password field is required!"
+//       document.loginForm.pwd.focus();
+//       return false;
+//     }
 
-    else{
-        loginMessage.innerHTML = `<img src="../Assets/loading1.gif" alt="" width="8%">`
-        registeredEmailMessage.style.display = "none"
-        registeredPasswordMessage.style.display = "none"
-        for(let i=0; i<registeredUsers.length; i++){
-            const usersArray = registeredUsers[i]
+//     else{
+//         loginMessage.innerHTML = `<img src="../Assets/loading1.gif" alt="" width="8%">`
+//         registeredEmailMessage.style.display = "none"
+//         registeredPasswordMessage.style.display = "none"
+//         for(let i=0; i<registeredUsers.length; i++){
+//             const usersArray = registeredUsers[i]
             
-            if(registeredEmail.value != usersArray.userEmail){
-                loginMessage.style.display = "block"
-                loginMessage.innerHTML = "Invalid credentials!"
-            }
+//             if(registeredEmail.value != usersArray.userEmail){
+//                 loginMessage.style.display = "block"
+//                 loginMessage.innerHTML = "Invalid credentials!"
+//             }
 
-            else if(registeredPassword.value != usersArray.userPassword){
-                loginMessage.style.display = "block" 
-                loginMessage.innerHTML = "Invalid credentials!"
-            }
+//             else if(registeredPassword.value != usersArray.userPassword){
+//                 loginMessage.style.display = "block" 
+//                 loginMessage.innerHTML = "Invalid credentials!"
+//             }
 
-            else{
-                loginMessage.style.display = "block" 
-                loginMessage.innerHTML = "Success!"
-                loginMessage.style.color = "green"
-                localStorage.setItem("token", "asyfgjahjghvjkhjgaurgfivhjhfjwhufhsytdufghjtdxycfuvgibhtxdycfuvgdfgvhgvjhdgdsgfdsdf")
-                var result = registeredUsers.find(function(e) {
-                    return e.userEmail == usersArray.userEmail;
-                  });
-                const loggedInUser = {
-                    "firstName": result.firstName,
-                    "lastName": result.lastName,
-                    "userEmail": result.userEmail,
-                    "userPassword": result.userPassword,
-                }
-                localStorage.setItem("LoggedInUser", JSON.stringify(loggedInUser))
-                location = "adminPanel.html"
-            }
-        }
+//             else{
+//                 loginMessage.style.display = "block" 
+//                 loginMessage.innerHTML = "Success!"
+//                 loginMessage.style.color = "green"
+//                 localStorage.setItem("token", "asyfgjahjghvjkhjgaurgfivhjhfjwhufhsytdufghjtdxycfuvgibhtxdycfuvgdfgvhgvjhdgdsgfdsdf")
+//                 var result = registeredUsers.find(function(e) {
+//                     return e.userEmail == usersArray.userEmail;
+//                   });
+//                 const loggedInUser = {
+//                     "firstName": result.firstName,
+//                     "lastName": result.lastName,
+//                     "userEmail": result.userEmail,
+//                     "userPassword": result.userPassword,
+//                 }
+//                 localStorage.setItem("LoggedInUser", JSON.stringify(loggedInUser))
+//                 location = "adminPanel.html"
+//             }
+//         }
 
-  }
+//   }
 
+const URL = "http://localhost:5000/api/login";
+fetch(URL, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    email: registeredEmail.value,
+    password: registeredPassword.value
+  })
+})
+  .then(res => {
+    if (res.status === 400) {
+      return res.json().then(data => {
+        // console.log(data.InvalidCredentials)
+        loginMessage.style.display = "block" 
+        loginMessage.style.color = "red"
+        loginMessage.innerHTML = data.InvalidCredentials
+        throw new Error(data.error);
+        
+      });
+    }
+    if (!res.ok) {
+        console.log(res)
+      throw new Error(`HTTP error! status: ${res.status}`);
+      
+    }
+    return res.json();
+  })
+  .then(data => {
+    loginMessage.style.display = "block" 
+    loginMessage.style.color = "green"
+    loginMessage.innerHTML = data.successMessage
+    console.log(data )
+    // successful login, process the data
+  })
+  .catch(error => {
+    console.error(error);
+    // display the error message to the user
+  });
 }
 
 
 //Prevent access to the login page when logged in
 
-const userToken = localStorage.getItem("token")
-if(userToken){
-    location = "index.html"
-}
+// const userToken = localStorage.getItem("token")
+// if(userToken){
+//     location = "index.html"
+// }
