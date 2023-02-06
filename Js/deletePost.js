c09e723b0c740e5a070f48bc144bc15d047fda83
Luciefifi <niyomutonilucie@gmail.com>
2023-02-06 16:33:36 +0300

    ft(delete): -create deletepost.js file -modify managepost.js modify managepost.html -[finishes#008]

---
+++ Js/deletePost.js
@@ -0,0 +1,29 @@
+let postIdDeletion;
+const popupBox = document.getElementById('popupBox')
+
+function openPopup(post_id){
+    popupBox.classList.add("open-popup")
+    localStorage.setItem("postIdDeletion", post_id)
+    postIdDeletion = localStorage.getItem("postIdDeletion")
+}
+function closePopup(){
+    popupBox.classList.remove("open-popup")
+}
+
+
+ 
+async function deletePost(){
+    const getData = {
+        method: "DELETE",
+        headers: {"auth_token": JSON.parse(localStorage.getItem("token"))}
+    }
+    
+    let response = await fetch("http://localhost:5000/api/deleteBlog/"+postIdDeletion, getData)
+    const fetchedData = await response.json()
+    console.log(fetchedData)
+
+    if (fetchedData.successMessage){
+      location.reload()  
+    }
+
+}
