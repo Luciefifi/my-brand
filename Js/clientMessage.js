66223529300e634c23995e171eb7205b737a02fe
Luciefifi <niyomutonilucie@gmail.com>
2023-02-07 09:28:58 +0300

    ft(send message integration): -modify clientMessage.js file -modify contact.js file -modify index.js file -[finishes#009]

--- Js/clientMessage.js
+++ Js/clientMessage.js
@@ -1,64 +1,122 @@
 
-var allMessages = JSON.parse(localStorage.getItem('messages'))
-const clientContainer = document.getElementById('clientContainer')
+// var allMessages = JSON.parse(localStorage.getItem('messages'))
+// const clientContainer = document.getElementById('clientContainer')
 
-for (let i=0 ; i<allMessages.length; i++){
-     console.log(allMessages[i])
+// for (let i=0 ; i<allMessages.length; i++){
+//      console.log(allMessages[i])
 
-    const clientName = document.getElementById('clientName')
-    const clientNames = allMessages[i].Name
-    const clientEmail = document.getElementById('clientEmail')
-    const clientEmails = allMessages[i].Email
+//     const clientName = document.getElementById('clientName')
+//     const clientNames = allMessages[i].Name
+//     const clientEmail = document.getElementById('clientEmail')
+//     const clientEmails = allMessages[i].Email
 
-    const clientPhone = document.getElementById('clientPhone')
-    const clientPhones = allMessages[i].Tel
+//     const clientPhone = document.getElementById('clientPhone')
+//     const clientPhones = allMessages[i].Tel
 
-    const clientMessage = document.getElementById('clientMessage')
-    const clientMessages = allMessages[i].Message
+//     const clientMessage = document.getElementById('clientMessage')
+//     const clientMessages = allMessages[i].Message
 
     
 
-    clientContainer.innerHTML += `
-        <fieldset class="manageMessage">
+//     clientContainer.innerHTML += `
+//         <fieldset class="manageMessage">
             
-        <p class="userInfoBox">
-            <span>${clientNames}</span>
-            <span>${clientEmails}</span>
-            <span>${clientPhones}</span>
-        </p>
+//         <p class="userInfoBox">
+//             <span>${clientNames}</span>
+//             <span>${clientEmails}</span>
+//             <span>${clientPhones}</span>
+//         </p>
        
-        <p class="userMessageBox">${clientMessages}</p>
+//         <p class="userMessageBox">${clientMessages}</p>
 
        
-        <div class="editPost">
-            <p onclick = "storePostTitle('${clientName}')" style="color:green">Reply</p>
-            <p onclick="deleteMessage('${clientPhones}')" style="color:red"> <i class="fa fa-trash"></i> </p>
-        </div>
+//         <div class="editPost">
+//             <p onclick = "storePostTitle('${clientName}')" style="color:green">Reply</p>
+//             <p onclick="deleteMessage('${clientPhones}')" style="color:red"> <i class="fa fa-trash"></i> </p>
+//         </div>
         
-    </fieldset> 
-    `
-}
+//     </fieldset> 
+//     `
+// }
 
 
 
-function deleteMessage(visitorTel){
-    var client = allMessages.find(function(e)
-    {
-        return e.clientPhones = allMessages.visitorTel;
+// function deleteMessage(visitorTel){
+//     var client = allMessages.find(function(e)
+//     {
+//         return e.clientPhones = allMessages.visitorTel;
+//     });
+//     for (var j=0 ; j<allMessages.length; j++){
+//         var clients = allMessages[j];
+//         if(clients.phone == clients.clientPhones){
+//             allMessages.splice(j,1);
+//         }
+//     }
+//     allMessages = JSON.stringify(allMessages);
+//     localStorage.setItem("messages" , allMessages);
+//     history.go(0);
+
+// }
+
+
+
+const visitorName = document.getElementById('visitorName');
+const visitorLastName = document.getElementById('visitorLastName');
+const visitorEmail = document.getElementById('visitorEmail');
+const visitorMessage = document.getElementById('visitorMessage');
+const messageSubmitButton = document.getElementById('messageSubmitButton');
+const contactForm = document.getElementById('contactForm')
+const clientMessages = document.getElementById('clientMessages');
+
+clientMessages.style.display = "none";
+
+messageSubmitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  clientMessages.style.display = "block";
+  clientMessages.innerHTML = `<img src="../Assets/loading1.gif" alt="" width="8%">`;
+  contactMessage();
+});
+
+function contactMessage() {
+  const data = {
+    fname: visitorName.value,
+    lname: visitorLastName.value,
+    email: visitorEmail.value,
+    message: visitorMessage.value
+  };
+
+  const sendData = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: new Headers({'Content-Type': 'application/json; charset=UTF-8'})
+  };
+
+  fetch("http://localhost:5000/api/createMessage", sendData)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+
+      if (data.message) {
+        clientMessages.style.color = "red";
+        clientMessages.innerHTML = data.message;
+      } if (data.successMessage && data.message) {
+        clientMessages.style.color = "green";
+        clientMessages.innerHTML = data.successMessage;
+       
+
+
+        contactForm.reset()
+      setTimeout(()=>{location = "index.html"}, 2000)
+
+      } else if (data.validationError) {
+        clientMessages.style.color = "red";
+        clientMessages.innerHTML = data.validationError;
+      } else {
+        clientMessages.style.color = "red";
+        clientMessages.innerHTML = "Something went wrong, we were unable to register this account!";
+      }
     });
-    for (var j=0 ; j<allMessages.length; j++){
-        var clients = allMessages[j];
-        if(clients.phone == clients.clientPhones){
-            allMessages.splice(j,1);
-        }
-    }
-    allMessages = JSON.stringify(allMessages);
-    localStorage.setItem("messages" , allMessages);
-    history.go(0);
-
 }
-
-
 
 
 
--- Js/contact.js
+++ Js/contact.js
@@ -1,93 +1,93 @@
 
-const vistorName = document.getElementById("visitorName")
-const vistorEmail = document.getElementById('visitorEmail')
-const vistorTel = document.getElementById('visitorTel')
-const vistorMessage = document.getElementById('visitorMessage')
-const nameErrorMesssage = document.getElementById('nameErrorMessage')
-nameErrorMesssage.style.display="none"
-const emailErrorMesssage = document.getElementById('emailErrorMessage')
-emailErrorMesssage.style.display="none"
-const phoneErrorMessage = document.getElementById('phoneErrorMessage')
-phoneErrorMessage.style.display = "none"
-const errorMessage = document.getElementById("errorMessage")
-errorMessage.style.display = "none"
-const messageSentMessage = document.getElementById('messageSentMessage')
-messageSentMessage.style.display="none"
-const hireMeButton =  document.getElementById('messageSubmitButton')
+// const vistorName = document.getElementById("visitorName")
+// const vistorEmail = document.getElementById('visitorEmail')
+// const vistorTel = document.getElementById('visitorTel')
+// const vistorMessage = document.getElementById('visitorMessage')
+// const nameErrorMesssage = document.getElementById('nameErrorMessage')
+// nameErrorMesssage.style.display="none"
+// const emailErrorMesssage = document.getElementById('emailErrorMessage')
+// emailErrorMesssage.style.display="none"
+// const phoneErrorMessage = document.getElementById('phoneErrorMessage')
+// phoneErrorMessage.style.display = "none"
+// const errorMessage = document.getElementById("errorMessage")
+// errorMessage.style.display = "none"
+// const messageSentMessage = document.getElementById('messageSentMessage')
+// messageSentMessage.style.display="none"
+// const hireMeButton =  document.getElementById('messageSubmitButton')
 
 
 
-hireMeButton.addEventListener("click" , (event)=>{
-    event.preventDefault();
-    hireMe();
-})
-function hireMe() {
-  if (document.myForm.Name.value == "") {
-    nameErrorMesssage.style.display="block"
-    nameErrorMesssage.style.color = "red"
-    nameErrorMesssage.style.fontWeight = "bold"
-    nameErrorMesssage.innerHTML = "username can not be empty"
-    document.myForm.Name.focus();
+// hireMeButton.addEventListener("click" , (event)=>{
+//     event.preventDefault();
+//     hireMe();
+// })
+// function hireMe() {
+//   if (document.myForm.Name.value == "") {
+//     nameErrorMesssage.style.display="block"
+//     nameErrorMesssage.style.color = "red"
+//     nameErrorMesssage.style.fontWeight = "bold"
+//     nameErrorMesssage.innerHTML = "username can not be empty"
+//     document.myForm.Name.focus();
    
-  }
+//   }
 
-  var emailID = document.myForm.email.value;
-  atpos = emailID.indexOf("@");
-  dotpos = emailID.lastIndexOf(".");
+//   var emailID = document.myForm.email.value;
+//   atpos = emailID.indexOf("@");
+//   dotpos = emailID.lastIndexOf(".");
 
- if ((atpos < 1 || dotpos - atpos < 2) && emailID == "") {
-    emailErrorMesssage.style.display="block"
-    emailErrorMesssage.style.color = "red"
-    emailErrorMesssage.style.fontWeight = "bold"
-    emailErrorMesssage.innerHTML = "enter valid email address!"
+//  if ((atpos < 1 || dotpos - atpos < 2) && emailID == "") {
+//     emailErrorMesssage.style.display="block"
+//     emailErrorMesssage.style.color = "red"
+//     emailErrorMesssage.style.fontWeight = "bold"
+//     emailErrorMesssage.innerHTML = "enter valid email address!"
     
 
-    document.myForm.email.focus();
-  }
+//     document.myForm.email.focus();
+//   }
 
-  else if (document.myForm.telephone.value == "") {
-    alert("please provide your phone number!");
-    document.myForm.telephone.focus();
+//   else if (document.myForm.telephone.value == "") {
+//     alert("please provide your phone number!");
+//     document.myForm.telephone.focus();
     
-  }
-  else if (document.myForm.message.value == 0) {
-    alert("please provide your message!");
-    document.myForm.message.focus();
+//   }
+//   else if (document.myForm.message.value == 0) {
+//     alert("please provide your message!");
+//     document.myForm.message.focus();
     
-  }
+//   }
   
  
  
- else{
+//  else{
     
-    var messages;
-    if(!localStorage.messages){
-        messages = []
-    }
-    else{
-        messages = JSON.parse(localStorage.messages)
-    }
+//     var messages;
+//     if(!localStorage.messages){
+//         messages = []
+//     }
+//     else{
+//         messages = JSON.parse(localStorage.messages)
+//     }
 
-    const message = {}
-    message.Name = visitorName.value;
-    message.Email = visitorEmail.value;
-    message.Tel = visitorTel.value;
-    message.Message = visitorMessage.value;
-    console.log(vistorName);
+//     const message = {}
+//     message.Name = visitorName.value;
+//     message.Email = visitorEmail.value;
+//     message.Tel = visitorTel.value;
+//     message.Message = visitorMessage.value;
+//     console.log(vistorName);
 
-    messages.push(message)
-    localStorage.messages = JSON.stringify(messages)
-    messageSentMessage.style.display = "block"
-    messageSentMessage.style.color = "green"
-    messageSentMessage.style.fontWeight = "bold"
-    messageSentMessage.innerHTML = "message sent !!!!"
+//     messages.push(message)
+//     localStorage.messages = JSON.stringify(messages)
+//     messageSentMessage.style.display = "block"
+//     messageSentMessage.style.color = "green"
+//     messageSentMessage.style.fontWeight = "bold"
+//     messageSentMessage.innerHTML = "message sent !!!!"
     
-      setTimeout(()=>{location="#home"},1000)
-      myForm.reset();
-      nameErrorMesssage.style.display="none"
-      emailErrorMesssage.style.display="none"
-      phoneErrorMessage.style.display = "none"
-      errorMessage.style.display = "none"
+//       setTimeout(()=>{location="#home"},1000)
+//       myForm.reset();
+//       nameErrorMesssage.style.display="none"
+//       emailErrorMesssage.style.display="none"
+//       phoneErrorMessage.style.display = "none"
+//       errorMessage.style.display = "none"
       
      
 
@@ -96,6 +96,6 @@ function hireMe() {
    
 
 
- }
+//  }
 
-}
+// }
