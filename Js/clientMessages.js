55341a66895ab733939136636510c0d14e92fb03
Luciefifi <niyomutonilucie@gmail.com>
2023-02-07 10:56:32 +0300

    ft(send message integration): -modify clientMessage.js file -modify contact.js file -modify index.js file -create a new file createMessage.js file -create clientMessages.js file [finishes#010]

---
+++ Js/clientMessages.js
@@ -0,0 +1,57 @@
+let clientContainer = document.getElementById('clientContainer')
+// const tokens = localStorage.getItem("token")
+
+
+
+const getData = {
+    method: "GET",
+    // headers: {"auth_token": JSON.parse(localStorage.getItem("tokens"))}
+}
+
+
+
+const URL = "http://localhost:5000/api/getAllMessages"
+fetch(URL)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then(data => {
+    console.log(data)
+ 
+for(let i=0; i< data.data.length; i++){
+    messagesArray = data.data[i]
+    console.log(messagesArray)
+    const firstName = messagesArray.fname;
+    const lastName = messagesArray.lname;
+    const email = messagesArray.email;
+    const message = messagesArray.message;
+
+    clientContainer.innerHTML += `
+    <fieldset class="manageMessage">
+        
+    <p class="userInfoBox">
+        <span>${firstName}</span>
+        <span>${lastName}</span>
+        <span>${email}</span>
+    </p>
+   
+    <p class="userMessageBox">${message}</p>
+   
+    <div class="editPost">
+        <p onclick = "storePostTitle('${firstName}')" style="color:green">Reply</p>
+        <p onclick="deleteMessage('${email}')" style="color:red"> <i class="fa fa-trash"></i> </p>
+    </div>
+    
+</fieldset> 
+`
+
+
+}
+
+  })
+//    console.log(posts);
+  .catch(error => console.log(error.message));
+
