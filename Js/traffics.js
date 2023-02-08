b95e5ecac33e984e615e9cb9db2b3676a0bad9f7
Luciefifi <niyomutonilucie@gmail.com>
2023-02-08 15:28:59 +0300

    ft(traffics): -modify blog.html -modify adminPanel.js file -modify addmin panel.htmlm file -modify clientMessages.js file -modifiy singleBlog.html file -[finishes #012]

--- Js/traffics.js
+++ Js/traffics.js
@@ -1,33 +1,91 @@
-const postsNumber = document.getElementById('postsNumber')
-postsNumber.style.paddingTop = "25px"
-const registeredUsers = document.getElementById('registeredUsers')
-registeredUsers.style.paddingTop= "25px"
-const messages = document.getElementById('messages')
-messages.style.paddingTop = "25px"
+
+
+
 const traffics = document.getElementById('traffics')
 traffics.style.paddingTop = "25px"
+let  availableUsers = ''
+async function allusers(){
 
-const allAvailablePosts = JSON.parse(localStorage.getItem("posts"))
-const allUsers = JSON.parse(localStorage.getItem("users"))
-const allMessages = JSON.parse(localStorage.getItem('messages'))
+   
+    const registeredUsers = document.getElementById('registeredUsers')
+     registeredUsers.style.paddingTop= "25px"
+    let response = await fetch("http://localhost:5000/api/getAllUsers")
+    const fetchedData = await response.json()
+     availableUsers = fetchedData.allUsers.length
+    console.log(availableUsers)
+    if(availableUsers.length<=1){
+        registeredUsers.innerHTML=availableUsers +  " Registered User "
+    }
+    else{
+        registeredUsers.innerHTML = availableUsers+ " Registered Users"
+    }
+
+}
+console.log(availableUsers)
+
+
+let allAvailablePosts = ''
+
+const URL = "http://localhost:5000/api/getAllBlogs"
+fetch(URL)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then(data => {
+    const postsNumber = document.getElementById('postsNumber')
+    postsNumber.style.paddingTop = "25px"
+    allAvailablePosts = data.data.length
 if(allAvailablePosts.length<=1){
-    postsNumber.innerHTML=allAvailablePosts.length +  " Post "
+    postsNumber.innerHTML=allAvailablePosts +  " Post "
 }
 else{
-    postsNumber.innerHTML=allAvailablePosts.length + " Posts"
+    postsNumber.innerHTML=allAvailablePosts+ " Posts"
 }
 
-if(allUsers.length<=1){
-    registeredUsers.innerHTML=allUsers.length +  " Registered User "
-}
-else{
-    registeredUsers.innerHTML = allUsers.length + " Registered Users"
-}
-if(allMessages.length<=1){
-    messages.innerHTML=allMessages.length +  " Message "
-}
-else{
-    messages.innerHTML=allMessages.length + " Messages"
-}
-traffics.innerHTML= " 23 vistors ," + allAvailablePosts.length +  " Post  , " + allUsers.length +  " Registered User , " + allMessages.length +  " Message , " + " 70 subscribers"
+    console.log(allAvailablePosts)
+   
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+let allAvailableMessages = ''
+  
+  fetch("http://localhost:5000/api/getAllMessages")
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(data => {
+        const messages = document.getElementById('messages')
+        messages.style.paddingTop = "25px"
+      allAvailableMessages = data.data.length
+      console.log(allAvailableMessages)
+      if(allAvailableMessages.length<=1){
+        messages.innerHTML=allAvailableMessages +  " message "
+    }
+    else{
+        messages.innerHTML=allAvailableMessages+ " messages"
+    }
+ 
+    })
+    .catch(error => console.log(error.message));
+
+    traffics.innerHTML= `23 vistors , ${allAvailablePosts} Post  , ${availableUsers} Registered User ,  ${allAvailableMessages} Message , 70 subscribers`
+
+
+// traffics.innerHTML= " 23 vistors ," + allAvailablePosts.length +  " Post  , " + allUsers.length +  " Registered User , " + allMessages.length +  " Message , " + " 70 subscribers"
+
+allusers();
+
+
+
+
+
+
 
